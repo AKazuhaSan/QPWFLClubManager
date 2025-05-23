fc54256581441db3a895b08b638f074ea9ea6558
AKazuhaSan <finderplus3901@163.com>
2025-05-24 01:08:57 +0800

    制作了navbar

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,1 +1,18 @@
+// 引入 Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+// 引入 Bootstrap JS（包含 Popper）
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 import './stylesheets/main.css'
+
+const officalWebstieButton = document.getElementById("OfficalButton")!
+const suggestionButton = document.getElementById("Suggestion")!
+
+officalWebstieButton.addEventListener('click', () => {
+    console.log("hello")
+    window.open('https://qphs.shwfl.edu.cn/', '_blank')
+})
+
+suggestionButton.addEventListener('click', () => {
+    window.open("Suggestion.html", '_blank')
+})
